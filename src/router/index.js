5e3fd8cafd79f01d2b7da148a58e025d98555c82
Matheus <mathengs@hotmail.com>
2021-11-28 00:07:47 -0300

    Added all paths to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import MyFiles from '../views/MyFiles.vue'
+import Favorites from '../views/Favorites.vue'
+import Bin from '../views/Bin.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +12,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/myFiles',
+    name: 'MyFiles',
+    component: MyFiles
+  },
+  {
+    path: '/favorites',
+    name: 'Favorites',
+    component: Favorites
+  },
+  {
+    path: '/bin',
+    name: 'Bin',
+    component: Bin
   },
   {
     path: '/about',
